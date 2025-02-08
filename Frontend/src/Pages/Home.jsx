edd198034ce944025b72daee4c2963240b58b1be
duc-minh-droid <ducminhcsp@gmail.com>
2025-02-08 14:39:54 +0000

    ya

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,8 +1,9 @@
+import Map from '../Components/map.jsx';
+
 const Home = () => {
     return (
         <div className='flex h-full items-center justify-center flex-col gap-4'>
-            <p className='text-4xl text-white'>Hello!</p>
-            <p className='text-xl text-white'>Make sure to get the tailwind extension if you haven't got it</p>
+            <Map />
         </div>
     );
 }
