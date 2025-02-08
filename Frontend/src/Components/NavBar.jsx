76bc3aeaa0dfb0b3de5355fdf216b51558e43edc
duc-minh-droid <ducminhcsp@gmail.com>
2025-02-08 21:48:32 +0000

    A

--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FaMapMarkerAlt, FaSearch, FaLayerGroup } from "react-icons/fa";
 import FormModal from "./FormModal";
+import WMHB from "./WMHB";
 
 function NavBar() {
   const [openModal, setOpenModal] = useState(false);
@@ -8,12 +9,7 @@ function NavBar() {
   return (
     <div className="fixed bottom-5 right-5 flex flex-col gap-4 z-50 pointer-events-auto">
       {/* Marker Icon (Opens Modal) */}
-      <button
-        onClick={() => setOpenModal(true)}
-        className="flex items-center justify-center w-14 h-14 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 transition"
-      >
-        <FaMapMarkerAlt size={24} />
-      </button>
+      <WMHB setOpenModal={setOpenModal} />
 
       {/* Search Icon */}
       <button className="flex items-center justify-center w-14 h-14 bg-gray-700 text-white rounded-full shadow-lg hover:bg-gray-800 transition">
