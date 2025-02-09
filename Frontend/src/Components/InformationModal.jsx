0d4f4cdcf8e7bb34a93384b1d41c0f45945c874f
duc-minh-droid <ducminhcsp@gmail.com>
2025-02-09 10:23:21 +0000

    Update InformationModal.jsx

--- Frontend/src/Components/InformationModal.jsx
+++ Frontend/src/Components/InformationModal.jsx
@@ -1,17 +1,15 @@
 import React from "react";
+import Modal from '@mui/material/Modal';
 
 function InformationModal({ open, handleClose, title, bodyText }) {
     return (
-        <div className="absolute top-20 left-1/2 transform -translate-x-1/2 bg-white p-5 rounded shadow-lg w-80 pointer-events-auto text-black">
-        <h2 className="text-xl font-bold mb-3">{title}</h2>
-        <p>{bodyText}</p>
-        <button
-            onClick={handleClose}
-            className="mt-4 bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition"
-        >
-            Close
-        </button>
-        </div>
+        <Modal open={open}
+        onClose={handleClose}>
+            <div className="absolute top-20 left-1/2 transform -translate-x-1/2 bg-white p-5 rounded shadow-lg w-80 pointer-events-auto text-black">
+            <h2 className="text-xl font-bold mb-3">{title}</h2>
+            <p>{bodyText}</p>
+            </div>
+        </Modal>
     );
 }
 
