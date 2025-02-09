e03ba55c675db9580d2aa97fa19fa72f3f3dc8eb
Adam O'Neill <adamo.mark25@gmail.com>
2025-02-09 00:24:13 +0000

    Implemented play button with remebering the state

--- Frontend/src/Components/TimeSlider.jsx
+++ Frontend/src/Components/TimeSlider.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "@mui/material/Slider";
+import PlayButton from "./PlayButton";
 
 function TimeSlider({ yearBounds, setYearBounds }) {
   const minYear = 1950;
   const maxYear = 2025;
   const minGap = 1; // Minimum 1-year gap
+
+  const [isPlaying, setIsPlaying] = useState(false); // Use by play button
+
   const handleChange = (event, newValue) => {
     // Ensure newValue is an array (MUI sometimes passes a single value)
     if (!Array.isArray(newValue)) return;
@@ -28,6 +32,37 @@ function TimeSlider({ yearBounds, setYearBounds }) {
     setYearBounds([start, end]);
   };
 
+  useEffect(() => {
+    let intervalId;
+  
+    if (isPlaying) {
+      setYearBounds((prevBounds) => {
+        const [start, currentYear] = prevBounds;
+        return currentYear === maxYear ? [minYear, minYear] : prevBounds;
+      });
+  
+      intervalId = setInterval(() => {
+        setYearBounds((prevBounds) => {
+          const [start, currentYear] = prevBounds;
+          if (currentYear < maxYear) {
+            return [start, currentYear + 1];
+          } else {
+            clearInterval(intervalId);
+            setIsPlaying(false);
+            return prevBounds;
+          }
+        });
+      }, 250);
+    }
+  
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [isPlaying, minYear, maxYear, setYearBounds, setIsPlaying]);
+  
+
   return (
     <div className="absolute bottom-5 left-1/2 -translate-x-1/2 bg-white w-3/5 p-4 rounded-lg shadow-lg text-center pointer-events-auto">
       <p className="text-gray-800 font-semibold">Select Year Range</p>
@@ -45,6 +80,7 @@ function TimeSlider({ yearBounds, setYearBounds }) {
 
       <div className="mt-2 flex justify-between text-sm text-gray-700">
         <span>Start Year: {yearBounds[0]}</span>
+        <PlayButton isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
         <span>End Year: {yearBounds[1]}</span>
       </div>
     </div>
